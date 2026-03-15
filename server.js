8052278b1f2287f62e24c41ba7e2d87d0fc480ef
Pritam Paul <pritampaulsdev@gmail.com>
2026-03-15 15:04:08 +0530

    custom middleware added

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
-const app = express()
-app.get("/", (req, res) => {
-    res.send("Server is up and running on port 3000! Ready to handle requests.");
-})
-app.listen(3000);
+const app = express();
+app.use("/welcome", (req, res, next) => {
+  req.user = "Guest";
+  next();
+});
+app.get("/welcome", (req, res) => {
+  res.send(`<h1>Welcomle ${req.user}</h1>`);
+});
+app.listen(3000, () => {
+  console.log("server starts");
+});
