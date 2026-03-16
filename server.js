07e118b3e279302f6301951b83eec8d6e23034b2
Pritam Paul <pritampaulsdev@gmail.com>
2026-03-16 13:05:48 +0530

    get post task done using express server

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
-app.use("/welcome", (req, res, next) => {
-  req.user = "Guest";
-  next();
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders.");
 });
-app.get("/welcome", (req, res) => {
-  res.send(`<h1>Welcomle ${req.user}</h1>`);
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
 });
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+app.post("/users", (req, res) => {
+  res.send("A new user has been added");
+});
+
 app.listen(3000, () => {
-  console.log("server starts");
+  console.log("Server is running on  http://localhost:3000");
 });
