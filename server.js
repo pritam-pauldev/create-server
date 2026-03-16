9b51d046c3c2bc7de8580b2f1253ca32b196caab
Pritam Paul <pritampaulsdev@gmail.com>
2026-03-16 15:07:53 +0530

    dynamic route request with params and query task done using express server

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const app = express();
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.");
-});
-app.post("/products", (req, res) => {
-  res.send("A new product has been added.");
-});
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-});
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created.");
-});
-app.get("/*splat", (req, res) => {
-  res.status(404).send("<h1>404- Page Not Found</h1>")
+
+app.get("/welcome/:username", (req, res) => {
+  const username = req.params.username;
+  const role = req.query.role;
+  res.send(`Welcome ${username}, your role is ${role}`);
 })
 
 app.listen(3000, () => {
