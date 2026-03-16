4bdbeff96e60ca0d0f2bf342137bfd3efb06b2c6
Pritam Paul <pritampaulsdev@gmail.com>
2026-03-16 19:11:11 +0530

    logging middleware using req.url and method task done using express server

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
 
-app.get("/welcome/:username", (req, res) => {
-  const username = req.params.username;
-  const role = req.query.role;
-  res.send(`Welcome ${username}, your role is ${role}`);
-})
+app.use((req, res, next) => {
+  const url = req.url;
+  const method = req.method;
+  console.log(`${method} request made to ${url}`);
+  next();
+});
+app.get("/products", (req, res) => {
+  res.send("Here is the list of all products.");
+});
+app.post("/products", (req, res) => {
+  res.send("A new product has been added");
+});
+app.get("/categories", (req, res) => {
+  res.send("Here is the list of all categories.");
+});
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created ");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on  http://localhost:3000");
