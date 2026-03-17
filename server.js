ae64072f9e815c585043f3335d9ed230565a9104
Pritam Paul <pritampaulsdev@gmail.com>
2026-03-17 13:34:55 +0530

    config route using router folder task done using express server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const routerProducts = require("./routes/products");
+const routerCategories = require("./routes/categories");
 
 app.use((req, res, next) => {
   const url = req.url;
@@ -7,18 +9,8 @@ app.use((req, res, next) => {
   console.log(`${method} request made to ${url}`);
   next();
 });
-app.get("/products", (req, res) => {
-  res.send("Here is the list of all products.");
-});
-app.post("/products", (req, res) => {
-  res.send("A new product has been added");
-});
-app.get("/categories", (req, res) => {
-  res.send("Here is the list of all categories.");
-});
-app.post("/categories", (req, res) => {
-  res.send("A new category has been created ");
-});
+app.use("/products", routerProducts);
+app.use("/categories", routerCategories);
 
 app.listen(3000, () => {
   console.log("Server is running on  http://localhost:3000");
