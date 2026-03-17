a2db0ea6a71a536044aa41cf9cc7728435231707
Pritam Paul <pritampaulsdev@gmail.com>
2026-03-17 14:13:19 +0530

    route using router folder task done using express server

--- routes/books.js
+++ routes/books.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 router.get("/", (req, res) => {
-    res.send("Here is the list of all products.");
+    res.send("Here is the list of books!");
 })
 router.post("/", (req, res) => {
-    res.send("A new product has been added");
+    res.send("Book has been added!");
 })
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 const express = require("express");
 const app = express();
-const routerProducts = require("./routes/products");
-const routerCategories = require("./routes/categories");
-
-app.use((req, res, next) => {
-  const url = req.url;
-  const method = req.method;
-  console.log(`${method} request made to ${url}`);
-  next();
-});
-app.use("/products", routerProducts);
-app.use("/categories", routerCategories);
-
+const routerBooks = require("./routes/books");
+app.use("/books", routerBooks);
 app.listen(3000, () => {
-  console.log("Server is running on  http://localhost:3000");
-});
+  console.log("Server is running in port 3000");
+})
